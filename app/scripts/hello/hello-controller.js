1f0a040a559b78fdceba89f32b8cf6a65acefde1
fcamblor <fcamblor@gmail.com>
2017-03-16 12:39:22 +0100

    Migrated utilities to typescript, involving introduction of lodash @types definition

--- app/scripts/hello/hello-controller.js
+++ app/scripts/hello/hello-controller.js
@@ -1,12 +1,14 @@
 
-angular.module('angular1-ts-migration').component('helloPage', {
-    template: "Hello <strong>world</strong><ul><li ng-repeat='nb in $ctrl.numbers'>{{nb}}</li></ul>",
-    bindings : {
-        numbers: '<'
-    },
-    controller: [function(){
-        console.log("In hello page controller");
+var utilMod = require(['hello/utilities'], function(utlMod) {
+    angular.module('angular1-ts-migration').component('helloPage', {
+        template: "Hello <strong>world</strong><ul><li ng-repeat='nb in $ctrl.numbers'>{{nb}}</li></ul>",
+        bindings : {
+            numbers: '<'
+        },
+        controller: [function(){
+            console.log("In hello page controller");
 
-        this.numbers = window.utilities.generateNumbers();
-    }]
+            this.numbers = utlMod.Utilities.generateNumbers();
+        }]
+    });
 });
---
+++ app/scripts/hello/utilities.ts
@@ -0,0 +1,5 @@
+
+
+export const Utilities = {
+    generateNumbers(){ return _.range(0, 10) }
+};
