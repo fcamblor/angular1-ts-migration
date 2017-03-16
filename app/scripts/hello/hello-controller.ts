4ba0a9a3373a86e3ccc67f8e21e0785ec5ea9991
fcamblor <fcamblor@gmail.com>
2017-03-16 14:39:07 +0100

    Migrated hello-controller to typescript, keeping the exact same structure and only referencing angular and requirejs @types (the requirejs @types should become useless soon)

--- app/scripts/hello/hello-controller.ts
+++ app/scripts/hello/hello-controller.ts
@@ -1,5 +1,6 @@
 
-var utilMod = require(['hello/utilities'], function(utlMod) {
+
+var utilMod = require(['hello/utilities'], function(utlMod: any) {
     angular.module('angular1-ts-migration').component('helloPage', {
         template: "Hello <strong>world</strong><ul><li ng-repeat='nb in $ctrl.numbers'>{{nb}}</li></ul>",
         bindings : {
